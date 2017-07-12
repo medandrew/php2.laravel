327a333c67ed72b4a702a7341f00ff937610a53b
Andrew <359747@mail.ru>
2017-07-12 21:25:04 +0700

    public

---
+++ public/js/scripts.js
@@ -0,0 +1,28 @@
+<<<<<<< b39f4256d9ae5e66734eccb2fbbc2c71e127cc5f
+$(document).ready(function(e) {
+
+/* Слайдер jcarousel */
+    $('.jcarousel')
+    .jcarousel({
+// Core configuration goes here
+    })
+    .jcarouselAutoscroll({
+        interval: 5000,
+        target: '+=1',
+        autostart: true
+    });
+=======
+$(document).ready(function(e) {
+
+/* Слайдер jcarousel */
+    $('.jcarousel')
+    .jcarousel({
+// Core configuration goes here
+    })
+    .jcarouselAutoscroll({
+        interval: 5000,
+        target: '+=1',
+        autostart: true
+    });
+>>>>>>> 1eb44b7e26383972a177bf6cd42112c431f4a1e2
+}); // конец ready
