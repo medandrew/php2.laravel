8a4d124543f3cbaa0ffe7ac81b6be624d2f60bf1
Andrew <359747@mail.ru>
2017-07-14 02:24:22 +0700

    js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< b39f4256d9ae5e66734eccb2fbbc2c71e127cc5f
 $(document).ready(function(e) {
 
 /* Слайдер jcarousel */
@@ -11,18 +10,4 @@ $(document).ready(function(e) {
         target: '+=1',
         autostart: true
     });
-=======
-$(document).ready(function(e) {
-
-/* Слайдер jcarousel */
-    $('.jcarousel')
-    .jcarousel({
-// Core configuration goes here
-    })
-    .jcarouselAutoscroll({
-        interval: 5000,
-        target: '+=1',
-        autostart: true
-    });
->>>>>>> 1eb44b7e26383972a177bf6cd42112c431f4a1e2
 }); // конец ready
